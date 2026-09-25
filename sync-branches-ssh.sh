fb13af36338d02a1c691d9df41878308b66cce06
Owen Zimmerman <owen@fyralabs.com>
2026-09-24 23:19:27 -0500

    feat: port terra-scripts repo scripts to terra-cli
    Signed-off-by: Owen Zimmerman <owen@fyralabs.com>

---
+++ sync-branches-ssh.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/bash
+
+# Usage example:
+# sync-branches-ssh.sh username f44
+
+# NOTE: Requires ripgrep
+
+# WARNING: This version of the script clones over SSH.
+
+if [[ "$3" == "-x" ]]; then
+  set -x
+fi
+
+NAME="$1"
+BRANCH="$2"
+WORKDIR="$HOME/git/terra"
+SYNC="sync-$BRANCH"
+SYNCBRANCH="$NAME/chore/$SYNC"
+SYNCDIR="$WORKDIR/frawhide-sync"
+CLONE="git clone --recurse-submodules -j$(nproc) https://github.com/terrapkg/packages.git -b $BRANCH $SYNC"
+
+function clean_files() {
+for file in $(rg updbranch --files-with-matches --glob '!backup/'); do
+DIRNAME=$(dirname $file)
+BASENAME=$(basename $DIRNAME)
+DIR=$(echo $DIRNAME | sed "s|/$BASENAME||g")
+mkdir -p backup/$DIR
+mv $DIRNAME backup/$DIR
+done
+for file in $(rg terra\\\\$releasever --files-with-matches --glob '!backup/'); do
+DIRNAME=$(dirname $file)
+mkdir -p backup/$DIRNAME
+mv $file -t backup/$DIRNAME
+done
+for file in $(rg fedora\/\\\\$releasever --files-with-matches --glob '!backup/'); do
+DIRNAME=$(dirname $file)
+mkdir -p backup/$DIRNAME
+mv $file -t backup/$DIRNAME
+done
+}
+
+if [[ ! -d "$WORKDIR" ]]; then
+  mkdir -p $WORKDIR
+fi
+
+if [[ "$PWD" != "$WORKDIR" ]]; then
+  cd $WORKDIR
+fi
+
+if [[ ! -d "$SYNCDIR" ]]; then
+  git clone --recurse-submodules -j$(nproc) git@github.com:terrapkg/packages.git -b frawhide "$SYNCDIR"
+  pushd $SYNCDIR
+  clean_files
+  popd
+else
+  pushd $SYNCDIR
+  git pull --no-edit
+  popd
+fi
+
+$CLONE || { rm -rf $SYNC && $CLONE; }
+
+pushd $SYNC
+git checkout -b $SYNCBRANCH
+clean_files
+rm -rf anda/
+cp -pr $SYNCDIR/anda -t $PWD
+cp -pr backup/* -t $PWD
+rm -rf backup
+git add .
+git commit -a -m "chore: Sync $BRANCH"
+git push origin HEAD:$SYNCBRANCH || git push -f origin HEAD:$SYNCBRANCH
+popd
